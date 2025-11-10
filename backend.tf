05f5252784da5a918ac26a8bd6740b671f5aa86e
root <root@ip-172-31-11-44.ec2.internal>
2025-11-10 12:15:20 +0000

    Remove duplicate Terraform files and fix structure

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-25"  
-    key            = "infra/terraform.tfstate"       
-    region         = "us-east-2"                     
-    encrypt        = true                            
-    dynamodb_table = "terraform-locks"              
-  }
-}

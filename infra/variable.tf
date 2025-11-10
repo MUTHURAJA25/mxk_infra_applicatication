690738b0e400db3e7a85caa4043617d12427ee8e
MUTHURAJA25 <142464451+MUTHURAJA25@users.noreply.github.com>
2025-11-10 23:23:35 +0530

    Update variable.tf

--- infra/variable.tf
+++ infra/variable.tf
@@ -1,16 +1,39 @@
 variable "aws_region" {
-  default = "ap-south-1"
+  description = "AWS region where resources will be created"
+  type        = string
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  description = "Type of EC2 instance"
+  type        = string
 }
 
 variable "key_name" {
-  description = "SSH key pair name"
+  description = "SSH key pair name to connect to EC2"
   type        = string
 }
 
 variable "project_name" {
-  default = "jenkins-aws-demo"
+  description = "Name of the project (used for tagging)"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+}
+
+variable "az1" {
+  description = "Availability Zone for subnet"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
 }

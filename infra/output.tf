5d53ad5851f4cf9eee20f7ab84bb4985a81c8d64
MUTHURAJA25 <142464451+MUTHURAJA25@users.noreply.github.com>
2025-11-11 12:54:16 +0530

    Update output.tf

--- infra/output.tf
+++ infra/output.tf
@@ -1,39 +1,50 @@
+# VPC / Networking
 output "vpc_id" {
-  description = "ID of the created VPC"
+  description = "VPC ID"
   value       = aws_vpc.main.id
 }
 
 output "public_subnet_id" {
-  description = "ID of the public subnet"
+  description = "Public subnet ID"
   value       = aws_subnet.public.id
 }
 
-output "internet_gateway_id" {
-  description = "ID of the Internet Gateway"
-  value       = aws_internet_gateway.igw.id
-}
-
 output "route_table_id" {
-  description = "ID of the public route table"
+  description = "Route table ID"
   value       = aws_route_table.public_rt.id
 }
 
+output "internet_gateway_id" {
+  description = "Internet Gateway ID"
+  value       = aws_internet_gateway.igw.id
+}
+
 output "security_group_id" {
-  description = "ID of the EC2 security group"
+  description = "EC2 security group ID"
   value       = aws_security_group.ec2_sg.id
 }
 
-output "ec2_instance_id" {
-  description = "ID of the EC2 instance"
+# EC2
+output "ec2_id" {
+  description = "EC2 instance ID"
   value       = aws_instance.web.id
 }
 
 output "ec2_public_ip" {
-  description = "Public IP of the EC2 instance"
+  description = "EC2 public IP"
   value       = aws_instance.web.public_ip
 }
 
-output "ec2_private_ip" {
-  description = "Private IP of the EC2 instance"
-  value       = aws_instance.web.private_ip
+output "ec2_public_dns" {
+  description = "EC2 public DNS"
+  value       = aws_instance.web.public_dns
+}
+
+# Useful meta
+output "project_name" {
+  value = var.project_name
+}
+
+output "aws_region" {
+  value = var.aws_region
 }
